a29b0f64c615963bdc0bf7743e0cf377c7951ebf
deadsinsomnia <07303266@usc.edu.ph>
2026-03-04 00:38:53 +0800

    todo

---
+++ TODO/TODO/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.ObjectModel;
+using TODO.Models;
+
+namespace TODO
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<TodoItem> todoItems;
+        TodoItem selectedItem;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            todoItems = new ObservableCollection<TodoItem>();
+            todoListView.ItemsSource = todoItems;
+        }
+
+        // ADD
+        private void OnAddClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(titleEntry.Text))
+            {
+                todoItems.Add(new TodoItem
+                {
+                    Title = titleEntry.Text,
+                    Details = detailsEditor.Text
+                });
+
+                ClearFields();
+            }
+        }
+
+        // SELECT ITEM
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem is TodoItem item)
+            {
+                selectedItem = item;
+                titleEntry.Text = item.Title;
+                detailsEditor.Text = item.Details;
+            }
+        }
+
+        // UPDATE
+        private void OnUpdateClicked(object sender, EventArgs e)
+        {
+            if (selectedItem != null)
+            {
+                selectedItem.Title = titleEntry.Text;
+                selectedItem.Details = detailsEditor.Text;
+
+                // Refresh ListView
+                todoListView.ItemsSource = null;
+                todoListView.ItemsSource = todoItems;
+
+                ClearFields();
+            }
+        }
+
+        // DELETE
+        private void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var item = button?.CommandParameter as TodoItem;
+
+            if (item != null)
+            {
+                todoItems.Remove(item);
+            }
+        }
+
+        private void ClearFields()
+        {
+            titleEntry.Text = string.Empty;
+            detailsEditor.Text = string.Empty;
+            selectedItem = null;
+            todoListView.SelectedItem = null;
+        }
+    }
+}
---
+++ TODO/TODO/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace TODO.Models
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public string Details { get; set; }
+    }
+}
